d7b1b3d703f1dc4fe3f409b7c6955f61b3d7a59f
WhiteCatDeveloper <whitecatsdeveloper@gmail.com>
2021-03-02 23:36:16 +0400

    Начало создания бд для хранения настроек приложения

--- app/src/main/java/com/whitecatdeveloper/scoringforthegameerudite/model/Settings.java
+++ app/src/main/java/com/whitecatdeveloper/scoringforthegameerudite/model/Settings.java
@@ -3,9 +3,12 @@ package com.whitecatdeveloper.scoringforthegameerudite.model;
 
 public class Settings {
     private Language language;
+    private MyBackgroundColors colors;
 
 
     private static Settings instance;
+
+    private Settings() {}
 
     public static Settings getInstance() {
         if (instance == null) instance = new Settings();
@@ -18,5 +21,13 @@ public class Settings {
 
     public void setLanguage(Language language) {
         this.language = language;
+    }
+
+    public MyBackgroundColors getColors() {
+        return colors;
+    }
+
+    public void setColors(MyBackgroundColors colors) {
+        this.colors = colors;
     }
 }
