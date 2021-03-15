839fecbf8d0497eb3abffd98f6ba5d787ef1e57d
WhiteCatDeveloper <whitecatsdeveloper@gmail.com>
2021-03-15 21:28:24 +0400

    Начало создания бд для хранения настроек приложения

--- app/src/main/java/com/whitecatdeveloper/scoringforthegameerudite/view/SettingsActivity.java
+++ app/src/main/java/com/whitecatdeveloper/scoringforthegameerudite/view/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.whitecatdeveloper.scoringforthegameerudite.view;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -29,6 +30,8 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) actionBar.hide();
         settings = Settings.getInstance();
         switchLanguage = findViewById(R.id.switchLanguage);
         radioButtonBlack = findViewById(R.id.radioButtonBlack);
@@ -37,8 +40,8 @@ public class SettingsActivity extends AppCompatActivity {
         radioButtonOrange = findViewById(R.id.radioButtonOrange);
         radioButtonGreen = findViewById(R.id.radioButtonGreen);
         radioButtonBlue = findViewById(R.id.radioButtonBlue);
-        getRadioButtonColor();
-        getLanguage();
+//        getRadioButtonColor();
+//        getLanguage();
     }
 
     private void getRadioButtonColor() {
