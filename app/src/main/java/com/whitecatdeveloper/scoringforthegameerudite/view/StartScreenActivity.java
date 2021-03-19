e4d9c6f1bb5801fcc500b18d5c8d82272c4e9a10
WhiteCatDeveloper <whitecatsdeveloper@gmail.com>
2021-03-19 23:09:09 +0400

    Начало создания бд для хранения настроек приложения

--- app/src/main/java/com/whitecatdeveloper/scoringforthegameerudite/view/StartScreenActivity.java
+++ app/src/main/java/com/whitecatdeveloper/scoringforthegameerudite/view/StartScreenActivity.java
@@ -3,9 +3,16 @@ package com.whitecatdeveloper.scoringforthegameerudite.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
 import com.whitecatdeveloper.scoringforthegameerudite.R;
+import com.whitecatdeveloper.scoringforthegameerudite.database.SettingsContract;
+import com.whitecatdeveloper.scoringforthegameerudite.database.SettingsDBHelper;
+import com.whitecatdeveloper.scoringforthegameerudite.model.Language;
+import com.whitecatdeveloper.scoringforthegameerudite.model.MyBackgroundColors;
 import com.whitecatdeveloper.scoringforthegameerudite.model.Settings;
 
 public class StartScreenActivity extends AppCompatActivity {
@@ -21,6 +28,18 @@ public class StartScreenActivity extends AppCompatActivity {
     }
 
     private void initDataBase () {
+        SettingsDBHelper dbHelper = new SettingsDBHelper(this);
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+        Cursor cursor = database.query(SettingsDBHelper.DB_NAME, null, null, null, null, null, null);
+        String language = null;
+        String color = null;
+        if (cursor.moveToFirst()) {
+            language = cursor.getString(cursor.getColumnIndex(SettingsContract.SettingsEntry.COLUMN_LANGUAGE));
+            color = cursor.getString(cursor.getColumnIndex(SettingsContract.SettingsEntry.COLUMN_BACKGROUND_COLORS));
+        } else {
+            language = String.valueOf(Language.CYRILLIC);
+            color = String.valueOf(MyBackgroundColors.GREEN);
+        }
 
     }
 
